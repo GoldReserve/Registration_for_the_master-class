90bd075e9bd955ee4307957580e516cf1ff98205
Александр Сапрыкин <alamana13@mail.ru>
2023-02-12 13:06:19 +0300

    Наконец-то дописал css и вывод чтобы это прилично смотрелось, теперь осталось сделать передачу в телеграм

--- master_class/index/forms.py
+++ master_class/index/forms.py
@@ -1,8 +1,18 @@
 from django import forms
 from .models import *
 
+GEEKS_CHOICES = (
+    ("1", "№1 Делаем с Риткой жаркое"),
+    ("2", "№2 Светодиодная подсветка"),
+    ("3", "№3 Выращиваем всякое вертикально"),
+    ("4", "№4 Деревянная шкатулка"),
+    ("5", "№5 Литье эпоксидной смолой"),
+)
+
+
 class MasterClassRegistration(forms.Form):
     name = forms.CharField(max_length=100, label='ФИО')
-    number = forms.IntegerField(label='Тел.')
-    date = forms.DateTimeField(label='Дата')
+    number = forms.IntegerField(label='Тел')
+    master_class = forms.ChoiceField(choices=GEEKS_CHOICES, label='Вид')
+    date = forms.CharField(label='Дата')
     email = forms.EmailField(label='Email')
--- master_class/index/views.py
+++ master_class/index/views.py
@@ -5,5 +5,14 @@ from index.forms import MasterClassRegistration
 
 # Create your views here.
 def index(request):
-    form = MasterClassRegistration()
-    return render(request, 'index/index.html', {'form' : form})
+    message = False
+    if request.method == 'POST':
+        form = MasterClassRegistration(request.POST)
+        if form.is_valid():
+            #Тут мы передаем form.cleaned_data в телегу
+            print(form.cleaned_data)
+            message = 'Вы успешно записались на мастер класс!'
+    else:
+        form = MasterClassRegistration()
+    return render(request, 'index/index.html', {'form': form, 'message': message, 'cleaned_data':form.cleaned_data})
+
